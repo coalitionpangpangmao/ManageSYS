0844a86fe50e1832ed5585bce584c8593e08335b
pangpangmao <wicky0607@163.com>
2018-06-09 08:59:09 +0800

    采用了数据库连接池的  数据库操作类

---
+++ MSYS.DAL/OracleOperator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.OracleClient;
+using System.Data.Common;
+using System.Data;
+
+namespace MSYS.DAL
+{
+    public class OracleOperator : IDbOperator
+    {
+        public OracleOperator()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+        public string UpDate(string query)
+        {
+            DbCommand sqlcom = null;
+            DbConnection myConn = null;
+            IFactoryDbPool pool = OracleConnectionSingletion.CreateInstance();
+            try
+            {
+                ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["OracleConnectionString"];
+                OracleConnectionSingletion.ConnectionString = settings.ConnectionString;
+                //Borrow the SqlConnection object from the pool
+                myConn = pool.BorrowDBConnection();
+                sqlcom = new OracleCommand(query, (OracleConnection)myConn);
+                sqlcom.ExecuteNonQuery();
+                return "Success";
+            }
+            catch (Exception ee)
+            {
+                if (sqlcom != null)
+                    sqlcom.Dispose();
+                return ee.Message;
+            }
+            finally
+            {
+                pool.ReturnDBConnection(myConn);
+            }
+        }
+        public DataSet CreateDataSet(string query)
+        {
+            DbCommand sqlcom = null;
+            DbConnection myConn = null;
+            IFactoryDbPool pool = OracleConnectionSingletion.CreateInstance();
+            try
+            {
+                ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["OracleConnectionString"];
+                OracleConnectionSingletion.ConnectionString = settings.ConnectionString;
+                //Borrow the SqlConnection object from the pool
+                myConn = pool.BorrowDBConnection();
+                sqlcom = new OracleCommand(query, (OracleConnection)myConn);
+
+                DataSet ds = new DataSet();
+                OracleDataAdapter oda = new OracleDataAdapter();
+                oda.SelectCommand = (OracleCommand)sqlcom;
+                oda.Fill(ds);
+                return ds;
+            }
+            catch (Exception)
+            {
+                if (sqlcom != null)
+                    sqlcom.Dispose();
+                return null;
+            }
+            finally
+            {
+                pool.ReturnDBConnection(myConn);
+            }
+        }
+
+    }
+}
---
+++ MSYS.DAL/SQLOperator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data;
+
+namespace MSYS.DAL
+{
+    public class SQLOperator : IDbOperator
+    {
+        public SQLOperator()
+        {
+            //
+            //TODO: 在此处添加构造函数逻辑
+            //
+        }
+        public string UpDate(string query)
+        {
+            DbCommand sqlcom = null;
+            DbConnection myConn = null;
+            IFactoryDbPool pool = SQLConnectionSingletion.CreateInstance();
+            try
+            {
+                ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["SQLConnectionString"];
+                SQLConnectionSingletion.ConnectionString = settings.ConnectionString;
+                //Borrow the SqlConnection object from the pool
+                myConn = pool.BorrowDBConnection();
+                sqlcom = new SqlCommand(query, (SqlConnection)myConn);
+                sqlcom.ExecuteNonQuery();
+                return "Success";
+            }
+            catch (Exception ee)
+            {
+                if (sqlcom != null)
+                    sqlcom.Dispose();
+                return ee.Message;
+            }
+            finally
+            {
+                pool.ReturnDBConnection(myConn);
+            }
+        }
+        public DataSet CreateDataSet(string query)
+        {
+            DbCommand sqlcom = null;
+            DbConnection myConn = null;
+            IFactoryDbPool pool = SQLConnectionSingletion.CreateInstance();
+            try
+            {
+                ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["OracleConnectionString"];
+                SQLConnectionSingletion.ConnectionString = settings.ConnectionString;
+                //Borrow the SqlConnection object from the pool
+                myConn = pool.BorrowDBConnection();
+                sqlcom = new SqlCommand(query, (SqlConnection)myConn);
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter oda = new SqlDataAdapter();
+                oda.SelectCommand = (SqlCommand)sqlcom;
+                oda.Fill(ds);
+                return ds;
+            }
+            catch (Exception)
+            {
+                if (sqlcom != null)
+                    sqlcom.Dispose();
+                return null;
+            }
+            finally
+            {
+                pool.ReturnDBConnection(myConn);
+            }
+        }
+
+    }
+}
