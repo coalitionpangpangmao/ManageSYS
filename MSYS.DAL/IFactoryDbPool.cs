9ed918a9e582ea8b6448069f1e056be16c5b4a04
pangpangmao <wicky0607@163.com>
2018-06-09 08:58:13 +0800

    数据连接缓存池接口

---
+++ MSYS.DAL/IFactoryDbPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Common;
+namespace MSYS.DAL
+{
+    public interface IFactoryDbPool
+    {  
+        DbConnection BorrowDBConnection();  
+        void ReturnDBConnection(DbConnection conn);
+         IFactoryDbPool CreateInstanceI();
+    }
+
+}
