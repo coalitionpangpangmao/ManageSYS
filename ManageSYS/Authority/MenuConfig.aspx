554182b0197c6603548b8e25a36caa55d5e8f4aa
pangpangmao <wicky0607@163.com>
2018-06-12 09:11:06 +0800

    建立网页地址URL与MAPID映射表，便于统一管理

---
+++ ManageSYS/Authority/MenuConfig.aspx
@@ -0,0 +1,152 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MenuConfig.aspx.cs" Inherits="Authority_GroupConfig" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>菜单项配置</title>
+    <link href="../css/style.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="../js/jquery.js"></script>
+    <script type="text/javascript">
+        function selChange() {
+            debugger;
+            var str = $("#listType").val()
+            if ($("#listType").val() == 0)
+                $("#prtMenu").show();
+            else
+                $("#prtMenu").hide();
+            $("#hide").val($("#listType").val());
+            str = $("#hide").val();
+        }
+        $().ready(function () { $("#gridPanel").scrollTop = $("#hdScrollY").val(); });
+        function saveScroll() {
+            var y = $("#gridPanel").scrollTop();
+            $("#hdScrollY").val(y);
+        }
+   
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="place">
+        <span>位置：</span>
+        <ul class="placeul">
+            <li><a href="#">权限管理</a></li>
+            <li><a href="#">菜单配置</a></li>
+        </ul>
+    </div>
+    <div class="formbody">
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+        </asp:ScriptManager>
+        <div id="usual1" class="usual">
+            <div>
+                <asp:UpdatePanel ID="UpdatePanel2" runat="server" UpdateMode="Conditional" ChildrenAsTriggers="True">
+                    <ContentTemplate>
+                        <asp:HiddenField ID="hdScrollY" runat="server" />
+                        <table>
+                            <tr>
+                                <td  >
+                                <div style="border: thin solid #a7b5bc; height: 180px; overflow: scroll;" valign="top">
+                                    <asp:TreeView ID="RightTree" runat="server" Width="150px" OnSelectedNodeChanged="RightTree_SelectedNodeChanged">
+                                    </asp:TreeView></div>
+                                </td>
+                                <td>
+                                    <ul class="forminfo">
+                                        <li>
+                                            <label>
+                                                菜单名<b>*</b></label><asp:TextBox ID="txtMenu" runat="server" class="dfinput" Style="width: 518px;"></asp:TextBox></li>
+                                        <li>
+                                            <label>
+                                                菜单级数<b>*</b></label><asp:DropDownList ID="listLevel" runat="server" class="drpdwnlist"
+                                                    Height="25px" Width="518px">
+                                                    <asp:ListItem></asp:ListItem>
+                                                    <asp:ListItem Value='1'>一级</asp:ListItem>
+                                                    <asp:ListItem Value='2'>二级</asp:ListItem>
+                                                </asp:DropDownList>
+                                        </li>
+                                        <li>
+                                            <label>
+                                                父菜单<b>*</b></label><asp:DropDownList ID="listPrt" runat="server" class="drpdwnlist"
+                                                    Height="25px" Width="518px">
+                                                </asp:DropDownList>
+                                            <li>
+                                                <label>
+                                                    &nbsp;</label>&nbsp;&nbsp;
+                                                <asp:Button ID="btnSave2" runat="server" Text="保存" class="btnview" OnClick="btnSave2_Click" />
+                                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                                <asp:Button ID="btnDel" runat="server" Text="删除" class="btndel" OnClick="btnDel_Click" />
+                                                &nbsp;&nbsp;&nbsp;&nbsp; </li>
+                                    </ul>
+                                </td>
+                        </table>
+                    </ContentTemplate>
+                    <Triggers>
+                        <asp:AsyncPostBackTrigger ControlID="btnSave2" />
+                        <asp:AsyncPostBackTrigger ControlID="btnDel" />
+                        <asp:AsyncPostBackTrigger ControlID = "RightTree" />
+                    </Triggers>
+                </asp:UpdatePanel>
+            </div>
+            <div class="listtitle">
+                页面URL映射<span style="position: relative; float: right">
+                    <asp:Button ID="btnAdds" runat="server" CssClass="btnadd" Text="新增" OnClick="btnAdds_Click" />
+                </span>
+            </div>
+            <div id="gridPanel" onscroll="saveScroll()" style="height: 300px; overflow: scroll">
+                <asp:UpdatePanel ID="updtpanel1" runat="server">
+                    <ContentTemplate>
+                        <asp:GridView ID="GridView1" runat="server" class="grid" AutoGenerateColumns="False"
+                            DataKeyNames="MapID">
+                            <Columns>
+                                <asp:TemplateField HeaderText="MapID">
+                                    <ItemTemplate>
+                                        <asp:TextBox ID="txtID" runat="server" DataValueField="MapID" DataTextField="MapID"
+                                            Width="50px" CssClass="tbinput" Enabled="False"></asp:TextBox>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="父菜单">
+                                    <ItemTemplate>
+                                        <asp:DropDownList ID="listPrt" runat="server" CssClass="drpdwnlist" Width="80px"
+                                            DataSource="<%#bindprt() %>" DataTextField="NAME" DataValueField="ID">
+                                        </asp:DropDownList>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="菜单名称">
+                                    <ItemTemplate>
+                                        <asp:TextBox ID="txtMenu" runat="server" DataValueField="菜单名称" DataTextField="菜单名称"
+                                            CssClass="tbinput1"></asp:TextBox>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="URL">
+                                    <ItemTemplate>
+                                        <asp:TextBox ID="txtURL" runat="server" DataValueField="URL" DataTextField="URL"
+                                            Width="250px" CssClass="tbinput"></asp:TextBox>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="描述">
+                                    <ItemTemplate>
+                                        <asp:TextBox ID="txtDscrp" runat="server" DataValueField="描述" DataTextField="描述"
+                                            Width="250px" CssClass="tbinput"></asp:TextBox>
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                                <asp:TemplateField HeaderText="操作" HeaderStyle-HorizontalAlign="Center">
+                                    <ItemTemplate>
+                                        <asp:Button ID="btnSave" runat="server" Text="保存" CssClass="btn1" OnClick="btnSave_Click" />
+                                        <asp:Button ID="btnDelete" runat="server" Text="删除" CssClass="btn1" OnClick="btnDelete_Click" />
+                                    </ItemTemplate>
+                                </asp:TemplateField>
+                            </Columns>
+                            <HeaderStyle CssClass="gridheader" />
+                            <RowStyle CssClass="gridrow" />
+                        </asp:GridView>
+                    </ContentTemplate>
+                    <Triggers>
+                        <asp:AsyncPostBackTrigger ControlID="btnAdds" />
+                        <asp:AsyncPostBackTrigger ControlID="GridView1" />
+                    </Triggers>
+                </asp:UpdatePanel>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ManageSYS/Authority/MenuConfig.aspx.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+public partial class Authority_GroupConfig : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bindData();
+            BindList(RightTree.Nodes, "");
+            DataBaseOperator opt = new DataBaseOperator();
+            opt.bindDropDownList(listPrt, "select * from ht_svr_prt_menu where IS_DEL = '0' order by ID", "NAME", "ID");
+
+        }
+
+    }
+
+    protected void bindData()
+    {
+        string query = "select t.mapid as MapID,r.f_pid as 父菜单,r.f_menu as 菜单名称,t.url as URL,t.remark  as 描述 from ht_inner_map t left join ht_svr_sys_menu r on r.f_mapid = t.mapid and r.f_type = '0' where t.is_del = '0'  order by t.mapid ";
+        DataBaseOperator opt = new DataBaseOperator();
+        DataSet data = opt.CreateDataSetOra(query);
+        GridView1.DataSource = data;
+        GridView1.DataBind();
+        if (data != null && data.Tables[0].Rows.Count > 0)
+        {
+            for (int i = 0; i <= GridView1.Rows.Count - 1; i++)
+            {
+                DataRowView mydrv = data.Tables[0].DefaultView[i];
+                ((DropDownList)GridView1.Rows[i].FindControl("listPrt")).SelectedValue = mydrv["父菜单"].ToString();
+                ((TextBox)GridView1.Rows[i].FindControl("txtID")).Text = mydrv["MapID"].ToString();
+                ((TextBox)GridView1.Rows[i].FindControl("txtMenu")).Text = mydrv["菜单名称"].ToString();
+                ((TextBox)GridView1.Rows[i].FindControl("txtURL")).Text = mydrv["URL"].ToString();
+                ((TextBox)GridView1.Rows[i].FindControl("txtDscrp")).Text = mydrv["描述"].ToString();
+            }
+        }
+
+    }
+    protected void btnAdds_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string query = "select t.mapid as MapID,r.f_pid as 父菜单,r.f_menu as 菜单名称,t.url as URL,t.remark  as 描述 from ht_inner_map t left join ht_svr_sys_menu r on r.f_mapid = t.mapid and r.f_type = '0' where t.is_del = '0'  order by t.mapid ";
+            DataBaseOperator opt = new DataBaseOperator();
+            DataSet set = opt.CreateDataSetOra(query);
+            DataTable data = new DataTable();
+            string newMapID = (Convert.ToInt16(opt.GetSegValue("select max(MapID) as ID from ht_inner_map", "ID")) + 1).ToString().PadLeft(5, '0');
+
+            if (set == null)
+            {
+                data.Columns.Add("MapID");
+                data.Columns.Add("父菜单");
+                data.Columns.Add("菜单名称");
+                data.Columns.Add("URL");
+                data.Columns.Add("描述");
+            }
+            else
+                data = set.Tables[0];
+            object[] value = { newMapID, "", "", "", "" };
+            data.Rows.Add(value);
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+            if (data != null && data.Rows.Count > 0)
+            {
+                for (int i = 0; i <= GridView1.Rows.Count - 1; i++)
+                {
+                    DataRowView mydrv = data.DefaultView[i];
+                    ((DropDownList)GridView1.Rows[i].FindControl("listPrt")).SelectedValue = mydrv["父菜单"].ToString();
+                    ((TextBox)GridView1.Rows[i].FindControl("txtID")).Text = mydrv["MapID"].ToString();
+                    ((TextBox)GridView1.Rows[i].FindControl("txtMenu")).Text = mydrv["菜单名称"].ToString();
+                    ((TextBox)GridView1.Rows[i].FindControl("txtURL")).Text = mydrv["URL"].ToString();
+                    ((TextBox)GridView1.Rows[i].FindControl("txtDscrp")).Text = mydrv["描述"].ToString();
+                }
+            }
+        }
+        catch (Exception ee)
+        {
+            Response.Write(ee.Message);
+        }
+    }
+    protected void BindList(TreeNodeCollection ncTree, string pID)
+    {
+        ncTree.Clear();
+        DataBaseOperator opt = new DataBaseOperator();
+        string query = "select ID,Name,MenuLEVEL,PID from ht_svr_prt_menu ";
+        if (pID == "")
+            query += " where pid is null ";
+        else
+            query += " where PID = '" + pID + "'";
+        DataSet data = opt.CreateDataSetOra(query);
+        if (data != null && data.Tables[0].Rows.Count > 0)
+        {
+            DataRow[] Rows = data.Tables[0].Select();
+            foreach (DataRow row in Rows)
+            {
+                TreeNode pnode = new TreeNode(row["Name"].ToString(), row["ID"].ToString() + "_" + row["MenuLEVEL"].ToString() + "_" + row["PID"].ToString());
+                BindList(pnode.ChildNodes, row["ID"].ToString());
+                ncTree.Add(pnode);
+
+
+            }
+        }
+    }
+    protected void RightTree_SelectedNodeChanged(object sender, EventArgs e)
+    {
+        string[] array = RightTree.SelectedValue.Split('_');
+        txtMenu.Text = RightTree.SelectedNode.Text;
+        listLevel.SelectedValue = array[1];
+        listPrt.SelectedValue = array[2];
+    }
+    protected void btnSave2_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (txtMenu.Text == "")
+                ScriptManager.RegisterStartupScript(UpdatePanel2, this.GetType(), "", "alert('请输入菜单名');", true);
+            else
+            {
+                DataBaseOperator opt = new DataBaseOperator();
+                string prtID;
+                if ("NoRecord" == (prtID = opt.GetSegValue("select * from HT_SVR_PRT_MENU where NAME = '" + txtMenu.Text + "'", "ID")))
+                {
+                    prtID = (Convert.ToInt16(opt.GetSegValue("select Max(ID) as ID from HT_SVR_PRT_MENU ", "ID")) + 1).ToString().PadLeft(2, '0');
+                }
+                opt.UpDateOra("delete from HT_SVR_PRT_MENU where ID = '" + prtID + "'");
+                string[] seg = { "ID", "NAME", "PID", "MENULEVEL" };
+                string[] value = { prtID, txtMenu.Text, listPrt.SelectedValue, listLevel.SelectedValue };
+                opt.InsertData(seg, value, "HT_SVR_PRT_MENU");
+                BindList(RightTree.Nodes, "");
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+    protected void btnDel_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            DataBaseOperator opt = new DataBaseOperator();
+            string query = "delete from HT_SVR_PRT_MENU where NAME = '" + txtMenu.Text + "'";
+            opt.UpDateOra(query);
+            BindList(RightTree.Nodes, "");
+            SetBlank();
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        int rowIndex = ((GridViewRow)btn.NamingContainer).RowIndex;
+        string id = GridView1.DataKeys[rowIndex].Value.ToString();
+
+        GridViewRow row = GridView1.Rows[rowIndex];
+        string mapID, RightID;
+        DataBaseOperator opt = new DataBaseOperator();
+        ///在映射表中插入URL映射
+        if ("NoRecord" == (mapID = opt.GetSegValue("select * from ht_inner_map where URL = '" + ((TextBox)row.FindControl("txtURL")).Text + "'", "MAPID")))
+        {
+            mapID = id;
+            opt.InsertData(new string[] { "MAPID", "URL" }, new string[] { mapID, ((TextBox)row.FindControl("txtURL")).Text }, "HT_INNER_MAP");
+        }
+        //在权限表中插入菜单权限 
+        if ("NoRecord" == (RightID = opt.GetSegValue("select * from ht_svr_sys_menu where F_MENU = '" + ((TextBox)row.FindControl("txtMenu")).Text + "'", "F_ID")))
+        {
+            RightID = (Convert.ToInt16(opt.GetSegValue("select Max(F_ID) as ID from ht_svr_sys_menu ", "ID")) + 1).ToString().PadLeft(5, '0');
+        }
+        opt.UpDateOra("delete from   ht_svr_sys_menu  where f_ID = '" + RightID + "'");
+        string[] seg = { "F_ID", "F_MENU", "F_MAPID", "F_PID", "F_DESCRIPT", "F_TYPE" };
+        string[] value = { RightID, ((TextBox)row.FindControl("txtMenu")).Text, mapID, ((DropDownList)row.FindControl("listPrt")).SelectedValue, ((TextBox)row.FindControl("txtDscrp")).Text, "0" };
+        opt.InsertData(seg, value, "ht_svr_sys_menu");
+        bindData();
+
+
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        int rowIndex = ((GridViewRow)btn.NamingContainer).RowIndex;
+        string id = GridView1.DataKeys[rowIndex].Value.ToString();
+        DataBaseOperator opt = new DataBaseOperator();
+        string query = "update  ht_inner_map  set is_del = '1' where MAPID = '" + id + "'";
+        opt.UpDateOra(query);
+        query = "update ht_svr_sys_menu set IS_DEL = '1' where F_MENU = '" + ((TextBox)GridView1.Rows[rowIndex].FindControl("txtMenu")).Text + "'";
+        opt.UpDateOra(query);
+        bindData();
+    }
+
+
+    protected DataSet bindprt()
+    {
+        DataBaseOperator opt = new DataBaseOperator();
+        return opt.CreateDataSetOra("select * from ht_svr_prt_menu where IS_DEL = '0' order by ID");
+    }
+
+
+
+
+    protected void SetBlank()
+    {
+        txtMenu.Text = "";
+        listLevel.SelectedValue = "";
+        listPrt.SelectedValue = "";
+    }
+
+
+}
