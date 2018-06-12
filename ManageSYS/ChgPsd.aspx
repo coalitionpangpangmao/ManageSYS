3875258b3384d279d8e08d0f92081d7ffb3d1609
pangpangmao <wicky0607@163.com>
2018-06-12 15:20:50 +0800

    用户修改密码页面

---
+++ ManageSYS/ChgPsd.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ChgPsd.aspx.cs" Inherits="Authority_ChgPsd" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="js/jquery.js"></script>
+
+<script language="javascript" type = "text/javascript">
+    $(function () {
+        $('.error').css({ 'position': 'absolute', 'left': ($(window).width() - 490) / 2 });
+        $(window).resize(function () {
+            $('.error').css({ 'position': 'absolute', 'left': ($(window).width() - 490) / 2 });
+        })
+    });  
+</script> 
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+	<div class="place">
+    <span>位置：</span>
+    <ul class="placeul">
+    <li><a href="#">首页</a></li>
+    <li><a href="#">更改密码</a></li>
+    </ul>
+    </div>
+    
+   <div class="formbody">
+    
+    <div class="formtitle"><span>基本信息</span></div>
+    
+    <ul class="forminfo">
+     <li><label>旧密码</label><asp:TextBox ID="txtold" runat="server" CssClass = "dfinput" 
+             TextMode="Password"></asp:TextBox> <i></i></li>
+    <li><label>新密码</label><asp:TextBox ID="txtPwd1" runat="server" CssClass = "dfinput" 
+            TextMode="Password"></asp:TextBox> <i>建议用8位以上字母数字结合的密码</i></li>
+    <li><label>确认密码</label><asp:TextBox ID="txtpwd2" runat="server" 
+            CssClass = "dfinput" TextMode="Password"></asp:TextBox><i>请再次输入密码</i></li>
+   
+    <li><label>&nbsp;</label><asp:Button ID="btnConfirm" runat="server" Text="确认修改"   
+            CssClass = "btn" onclick="btnConfirm_Click"/></li>
+    </ul>
+    
+    
+    </div>
+  
+    </form>
+</body>
+</html>
---
+++ ManageSYS/ChgPsd.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MSYS.Data;
+using MSYS.Web;
+using System.Data;
+public partial class Authority_ChgPsd : MSYS.Web.BasePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnConfirm_Click(object sender, EventArgs e)
+    {
+        if (Session["User"] != null)
+        {
+            string userID = ((SysUser)Session["User"]).Id;
+            DbOperator opt = new DbOperator();
+            DataTable userTable = opt.CreateDataSetOra("select * from ht_svr_user t where t.id = '" + userID + "'").Tables[0];
+
+            if (userTable != null && userTable.Rows.Count > 0)
+            {
+                string oldPwd = userTable.Rows[0]["PASSWORD"].ToString();
+                if (oldPwd == MSYS.Security.Encrypt.GetMD5String(txtold.Text))
+                {
+                    if (txtPwd1.Text == txtpwd2.Text)
+                    {
+                        string userPwd = MSYS.Security.Encrypt.GetMD5String(txtPwd1.Text);
+                        opt.UpDateOra("update ht_svr_user set PASSWORD = '" + userPwd + "' where ID = '" + userID + "'");
+                        opt.InsertTlog(Session["UserName"].ToString(), Page.Request.UserHostName.ToString(), "修改密码");
+                        string returnUrl = Request["ReturnUrl"];
+                        if (string.IsNullOrEmpty(returnUrl))
+                            returnUrl = "default.aspx";
+                        Response.Redirect(returnUrl);
+                    }
+                }
+                else
+                    Response.Write("<script language=\"javascript\" type=\"text/javascript\">alert('原始密码输入有误，请再次输入密码！！'); </script>");
+            }
+            else
+            {
+                Response.Write("<script language=\"javascript\" type=\"text/javascript\">alert('密码输入有误，请再次输入密码！！'); </script>");
+            }
+
+        }
+    }
+}
